39c3c87d7bffcdc86fb9834784368104cc6d1619
Vasanchai Prakobkij <59070156@it.kmitl.ac.th>
2019-07-09 14:08:06 +0700

    Edit starting info

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
+console.log('--------------------------------START--------------------------------');
+
 require('./startup/logging')();
 require('./startup/setup')(app);
 require('./startup/db')();
--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,6 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
-    console.log(db);
     mongoose.connect(db, {useCreateIndex: true, useNewUrlParser: true })
       .then(() => winston.info(`Connected to ${db}...`));
   }
