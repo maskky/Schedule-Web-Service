b2b211e09e65626e917abe5aa31520514bd9d947
Vasanchai Prakobkij <59070156@it.kmitl.ac.th>
2019-07-09 10:45:33 +0700

    Change local db

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,13 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 
+// module.exports = function() {
+//     mongoose.connect('mongodb://localhost/wpm', {useCreateIndex: true, useNewUrlParser: true })
+//     .then(() => winston.info('Connected to MongoDB...'));
+// }
+
 module.exports = function() {
-    mongoose.connect('mongodb://localhost/wpm', {useCreateIndex: true, useNewUrlParser: true })
-    .then(() => winston.info('Connected to MongoDB...'));
-}
+    const db = config.get('db');
+    mongoose.connect(db)
+      .then(() => winston.info(`Connected to ${db}...`));
+  }
