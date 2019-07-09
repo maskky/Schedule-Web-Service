e786167620349bb1fe455d9ee4c5e8c69cfb6463
Vasanchai Prakobkij <59070156@it.kmitl.ac.th>
2019-07-09 13:57:34 +0700

    Delete connectDB method

--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,6 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-// module.exports = function() {
-//     mongoose.connect('mongodb://localhost/wpm', {useCreateIndex: true, useNewUrlParser: true })
-//     .then(() => winston.info('Connected to MongoDB...'));
-// }
-
 module.exports = function() {
     const db = config.get('db');
     console.log(db);
