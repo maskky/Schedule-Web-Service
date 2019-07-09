04279e6d7297703f1e62b17cfe690b7d6be2ad34
Vasanchai Prakobkij <59070156@it.kmitl.ac.th>
2019-07-09 11:44:47 +0700

    Set config file

--- startup/db.js
+++ startup/db.js
@@ -9,6 +9,7 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
+    console.log(db);
     mongoose.connect(db, {useCreateIndex: true, useNewUrlParser: true })
       .then(() => winston.info(`Connected to ${db}...`));
   }
